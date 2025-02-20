e453d29819ae6e11199149751b507dba1db0ec2b
Arthur Pontier-Valois <arthurpontiervalois@gmail.com>
2025-02-20 13:47:19 -0500

    Configure db:seed for User model

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+if Rails.env.development?
+  User.find_or_create_by!(email: 'user@localdev.me') do |u|
+    u.password = 'XXXXXX'
+  end
+end
