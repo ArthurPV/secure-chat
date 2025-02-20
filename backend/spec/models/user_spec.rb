1a026a12e9ef451bbe04158619834c5ab2f51cd0
Arthur Pontier-Valois <arthurpontiervalois@gmail.com>
2025-02-20 13:38:20 -0500

    Add test in user model

---
+++ backend/spec/models/user_spec.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'columns' do
+    context 'validations' do
+      context 'email' do
+        it 'required' do
+          expect(User.create(email: nil).valid?).to eq(false)
+        end
+
+        it 'valid' do
+          expect(User.create(email: 'test').valid?).to eq(false)
+        end
+
+        it 'unique' do
+          User.create!(email: 'user@localdev.me', password: 'password').valid?
+
+          expect(User.new(email: 'user@localdev.me', password: 'password').valid?).to eq(false)
+        end
+      end
+
+      context 'encryped_password' do
+        it 'required' do
+          expect(User.new(email: 'user@localdev.me').valid?).to eq(false)
+        end
+      end
+    end
+  end
+end
