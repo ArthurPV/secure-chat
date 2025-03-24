8aed4ea62a53a569fd0f7fde52f7652c22895f0c
ArthurPV <arthurpontiervalois@gmail.com>
2025-03-24 15:10:23 -0400

    Add destroy route on user_conversations_controller.rb

--- backend/app/controllers/user_conversations_controller.rb
+++ backend/app/controllers/user_conversations_controller.rb
@@ -1,8 +1,20 @@
 # frozen_string_literal: true
 
 class UserConversationsController < ApplicationController
+  include EntityConcern
+
+  before_action :set_entity, only: %i[destroy]
+
   def create
     if UserConversation.create(user_conversation_params)
+      head :no_content
+    else
+      head :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @entity.destroy
       head :no_content
     else
       head :unprocessable_entity
@@ -13,5 +25,9 @@ class UserConversationsController < ApplicationController
 
   def user_conversation_params
     params.require(:user_conversation).permit(:public_key, :private_key, participants: [])
+  end
+
+  def set_entity
+    super(UserConversation)
   end
 end
