4f9827a8213c819ed96f0398520d039b4fb11526
ArthurPV <arthurpontiervalois@gmail.com>
2025-04-01 06:59:18 -0400

    Fix lint.

--- backend/spec/fabricators/user_fabricator.rb
+++ backend/spec/fabricators/user_fabricator.rb
@@ -3,7 +3,7 @@
 Fabricator(:user) do
   password '123456'
   email Faker::Internet.email
-  username Faker::Internet.username(separators: ['_'])
+  username Faker::Internet.username(separators: [ '_' ])
   phone_number Faker::Number.number(digits: 10).to_s
   public_key Faker::Crypto.sha256
   private_key Faker::Crypto.sha256
